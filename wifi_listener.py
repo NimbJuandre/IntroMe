7441d48e78acb44ab56d4f1319f88d02fa681754
Juandre Nimb <64599836+NimbJuandre@users.noreply.github.com>
2023-10-10 23:06:56 +0200

    adhoc

--- wifi_listener.py
+++ wifi_listener.py
@@ -3,6 +3,11 @@ import subprocess
 import re
 import os
 import netifaces
+import time
+
+observed_mac_addresses = {}
+# Store the last time the code for removing devices ran
+last_cleanup_time = 0
 
 def get_current_network_interface():
     try:
@@ -13,7 +18,8 @@ def get_current_network_interface():
         for interface in interfaces:
             addresses = netifaces.ifaddresses(interface)
             if netifaces.AF_INET in addresses:
-                return interface 
+                # Format the interface name as "\Device\NPF_{GUID}"
+                return f"\\Device\\NPF_{interface}"
         
         # If no connected interface found, return None
         return None
@@ -51,39 +57,62 @@ def install_wireshark():
     print("4. Restart your computer if necessary.")
     print("5. Run this script again after installation.")
 
-def parse_packet(packet):
-    # Extract source MAC address from the packet
-    mac_address_match = re.search(r"Source: ([\w:]+)", packet)
-    if mac_address_match:
-        return mac_address_match.group(1)
-    return None
+def parse_mac_address(line):
+    # Parse the MAC addresses from the captured line based on the provided data format
+    # Assuming the MAC addresses are separated by spaces
+    parts = line.strip().split()
+    if len(parts) > 0:
+            return parts[0]
+    else:
+        return None
 
+def cleanup_devices(current_time, timeout):
+    for mac, timestamp in list(observed_mac_addresses.items()):
+        if current_time - timestamp > timeout:
+            print(f"Device disconnected - MAC address: {mac}")
+            del observed_mac_addresses[mac]
 
-def start_network_listener(interface):
+def start_network_listener(interface, timeout=3600):
+    global last_cleanup_time
+
     if not is_tshark_installed():
         install_wireshark()
         return
 
     try:
-        # Run tshark to capture wireless network traffic and print it to stdout
         command = [
             "tshark",
-            "-i", interface,  # Specify the network interface (e.g., "wlan0" for Wi-Fi)
-            "-T", "fields",  # Output fields, one per line
-            "-e", "frame.time",  # Timestamp of the packet
-            "-e", "wlan.sa",  # Source MAC address
-            # "-Y", "wlan",  # Filter for Wi-Fi packets
+            "-i", interface,          # Specify the network interface (e.g., "wlan0" for Wi-Fi)
+            "-T", "fields",           # Output fields, one per line
+            "-e", "wlan.sa",          # Source MAC address
+            "-e", "wlan.fc",  
+            "-e", "eth.src",
         ]
 
         # Start tshark as a subprocess, capturing and parsing packets continuously
         tshark_process = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
 
-        print("Listening for network devices (Ctrl+C to stop):")
-
+        print(f"Listening for network devices on interface {interface} (Ctrl+C to stop):")
         for line in tshark_process.stdout:
-            mac_address = parse_packet(line)
+            # Extract the MAC address from the captured line
+            mac_address = parse_mac_address(line)
+
             if mac_address:
-                print(f"Device MAC address: {mac_address}")
+                current_time = time.time()
+
+                if mac_address not in observed_mac_addresses:
+                    print(f"MAC addresses added: {mac_address}")
+                    print(f"Total MAC addresses: {len(observed_mac_addresses)}")
+
+                # Update the timestamp for the existing MAC address or add it if not present
+                observed_mac_addresses[mac_address] = current_time
+
+                # Check if 5 minutes have elapsed since the last cleanup
+                if current_time - last_cleanup_time >= 300:  # 300 seconds = 5 minutes
+                    current_total = len(observed_mac_addresses)
+                    cleanup_devices(current_time, timeout)
+                    last_cleanup_time = current_time  # Update the last cleanup time
+                    print(f"Total MAC addresses after cleanup: {len(observed_mac_addresses)}. Removed {current_total - len(observed_mac_addresses)} devices")
 
     except KeyboardInterrupt:
         print("\nListener stopped by the user.")
@@ -92,8 +121,7 @@ def start_network_listener(interface):
 
 if __name__ == "__main__":
     if platform.system() == "Windows":
-        # current_connected_interface = get_current_network_interface()
-        current_connected_interface = r"\Device\NPF_{D487526C-FDB1-44C9-B484-72BF575A2138}"
+        current_connected_interface = get_current_network_interface()
         start_network_listener(current_connected_interface)
     else:
         print("This script is intended for Windows only.")
